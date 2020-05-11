2ad380d4ac0f5c82bb103d509f55f4fd01d36dce
vampire16 <hakevaibhav16@gmail.com>
2020-05-11 08:47:31 +0530

    ADDED : Factory method for service class object

--- src/main/java/com/bridgelabz/service/CliniqueManagementFactory.java
+++ src/main/java/com/bridgelabz/service/CliniqueManagementFactory.java
@@ -1,4 +1,8 @@
 package com.bridgelabz.service;
 
 public class CliniqueManagementFactory {
+
+    public static CliniqueManagement createServiceObject(){
+        return new CliniqueManagementService();
+    }
 }
